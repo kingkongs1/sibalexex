1e31b0dcc5ce1f5df482f406464388f8f10d8eb6
ambler082 <zzzodg@gmail.com>
2021-05-21 16:11:37 +0900

    delete

--- jellyfish_warning/settings.py
+++ jellyfish_warning/settings.py
@@ -118,6 +118,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL ='/media/'
