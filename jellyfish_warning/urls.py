1589e6a67a5a281e0b9c6f935719647ce255b31d
ambler082 <zzzodg@gmail.com>
2021-05-23 01:36:51 +0900

    url

--- jellyfish_warning/urls.py
+++ jellyfish_warning/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('http://testbkj.azurewebsites.net/', include('catalog.urls')),
+    path('', include('catalog.urls')),
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
