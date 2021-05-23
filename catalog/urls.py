e5455a50b96009af5ce6d1b14b0a1c63b8bfb694
ambler082 <zzzodg@gmail.com>
2021-05-24 01:29:18 +0900

    css

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
