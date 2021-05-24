40357e21cdd91b191a77fb7bb98582890144b375
ambler082 <zzzodg@gmail.com>
2021-05-24 10:19:47 +0900

    code-change

--- jellyfish_warning/urls.py
+++ jellyfish_warning/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('http://testbkj.azurewebsites.net/', include('catalog.urls')),
+    path('https://testbkj.azurewebsites.net/', include('catalog.urls')),
     path('', include('catalog.urls')),
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
